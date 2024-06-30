e43489ef3f0143c92ce3237de61bfd8e6bfc81e3
BJoarder <bjoarder87@gmail.com>
2024-06-30 23:11:24 +0600

    footer added

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
-import Navbar from "../Navbar/Navbar";
 import Slider from "../Slider/Slider";
 
 
 const Home = () => {
   return (
-    <div>
-      <Navbar></Navbar>
+    <div>      
       <Slider></Slider>
     </div>
   );
